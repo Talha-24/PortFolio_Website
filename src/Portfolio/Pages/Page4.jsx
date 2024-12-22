f56e9b613923d7ec527a19256c902fca567b8f22
Mohammad Talha <talhabhatti82542@gmail.com>
2024-12-22 10:38:37 +0500

    Added_links

--- src/Portfolio/Pages/Page4.jsx
+++ src/Portfolio/Pages/Page4.jsx
@@ -4,7 +4,7 @@ export const Page4 = () => {
   return (
     <div id='pageFour'>
       <div id="astrome">
-        <img src="src\Portfolio\Pages\Components\Page_1 Comp\Astro Me.avif" alt="" />
+        <img src="https://static.wixstatic.com/media/f1c650_50b3eb1a161343a6a87b1fe48cb27f9a~mv2.png/v1/fill/w_613,h_916,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Astro%20Me.png" alt="" />
       </div>
         
     </div>
