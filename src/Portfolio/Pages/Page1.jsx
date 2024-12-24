3a15eb89b865fd9768d716bdf330456bcdff2f36
Mohammad Talha <talhabhatti82542@gmail.com>
2024-12-25 01:36:04 +0500

    Added Files in a New Folder

--- src/Portfolio/Pages/Page1.jsx
+++ src/Portfolio/Pages/Page1.jsx
@@ -42,7 +42,7 @@ useGSAP(function(){
     return (
         <div id="pageOne" onMouseMove={(e)=>{
             mouseMove(e);
-        }} ><img src="src\Portfolio\Pages\Components\Page_1 Comp\Astro Me.avif" alt="" />
+        }} ><img src="https://static.wixstatic.com/media/f1c650_50b3eb1a161343a6a87b1fe48cb27f9a~mv2.png/v1/fill/w_501,h_749,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Astro%20Me.png" alt="" />
            <Header/>
            <Dream/>
             <Tilttext tilttext={tilttext} />
